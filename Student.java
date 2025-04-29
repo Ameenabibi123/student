96ab6f1be7d48f562afc433882a6932fc8b04b84
Ameenabibi123 <ameenamahammadanavar8@gmail.com>
2025-04-29 11:38:17 +0530

    updation

--- Student.java
+++ Student.java
@@ -5,12 +5,17 @@ public class Student {
             String name = "John Doe";
             int rollNumber = 101;
             String course = "bca";
+            String Address = "hubli";
+            double phoneno  =123;
     
             // Display the details
             System.out.println("Student Details:");
             System.out.println("Name: " + name);
             System.out.println("Roll Number: " + rollNumber);
             System.out.println("course: " + course);
+            System.out.println("address: " + Address);
+            System.out.println("phone no: " + phoneno);
+
             
         }
     }
