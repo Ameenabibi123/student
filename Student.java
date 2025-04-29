3b06c75ed636c4c9bafb55c2e00e737b01111fce
Ameenabibi123 <ameenamahammadanavar8@gmail.com>
2025-04-29 11:28:22 +0530

    student

---
+++ Student.java
@@ -0,0 +1,19 @@
+public class Student {
+   
+        public static void main(String[] args) {
+            // Student details
+            String name = "John Doe";
+            int rollNumber = 101;
+            String course = "bca";
+    
+            // Display the details
+            System.out.println("Student Details:");
+            System.out.println("Name: " + name);
+            System.out.println("Roll Number: " + rollNumber);
+            System.out.println("course: " + course);
+            
+        }
+    }
+    
+
+
